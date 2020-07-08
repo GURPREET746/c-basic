a2e658763051e1641693dcc74f69b2bbf6a3eedb
GURPREET746 <60273071+GURPREET746@users.noreply.github.com>
2020-07-08 11:33:45 +0530

    Create adding_two_number.cpp

---
+++ adding_two_number.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+      int first_no,second_no,result;
+      cout<<"enter both the number"<<endl;
+      cin>>first_no>>second_no;
+      result=first_no+second_no;
+      cout<<"result is "<<result<<endl;
+}      
+   
