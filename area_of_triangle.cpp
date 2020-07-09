d3568155c1eda471dc8066db1fa17b2d74189978
GURPREET746 <60273071+GURPREET746@users.noreply.github.com>
2020-07-09 23:21:32 +0530

    Create area_of_triangle.cpp

---
+++ area_of_triangle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  float height,base,area;
+  cout<<"enter the height and base of triangle"<<endl;
+  cin>>height>>base;
+  area=0.5*base*height;
+  cout<<"the area is: "<<area<<endl;
+  return 0;
+}  
