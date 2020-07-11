b0d427e076904cc63c3bdda4febfff3d2c509521
GURPREET746 <60273071+GURPREET746@users.noreply.github.com>
2020-07-11 20:24:25 +0530

    Create find_greater_number.cpp

---
+++ find_greater_number.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+ int x,y;
+ cout<<"enter both the number which you want to check"<<endl;
+ cin>>x>>y;
+ if(x > y)
+    cout<<x<<" is greater than "<<y<<endl;
+ else
+    cout<<y<<" is greater than "<<x<<endl;   
+    
+} 
