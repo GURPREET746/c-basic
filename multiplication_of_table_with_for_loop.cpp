dce49c180fb6bdf5b3974f53b58346ec7b4b61a7
GURPREET746 <60273071+GURPREET746@users.noreply.github.com>
2020-07-14 10:06:00 +0530

    Create multiplication_of_table_with_for_loop.cpp

---
+++ multiplication_of_table_with_for_loop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  int number,i=1,result;
+  cout<<"enter the Number of which you want to table"<<endl;
+  cin>>number;
+  for(;i<=10;i++){
+    result=number*i;
+    cout<<number<<" "<<"*"<<" "<<i<<" "<<"="<<" "<<result<<endl;
+ }
+ }
