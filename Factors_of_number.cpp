077628a905c7cb435daebf9ab96eceda0eae1881
GURPREET746 <60273071+GURPREET746@users.noreply.github.com>
2020-07-14 13:44:45 +0530

    Update Factors_of_number.cpp

--- Factors_of_number.cpp
+++ Factors_of_number.cpp
@@ -7,7 +7,7 @@ int main(){
   cout<<"enter the number of which you want to find factorial"<<endl;
   cin>>num;
   cout<<"Factors are: ";
-  for(int i=2;i<num;i++){
+  for(int i=1;i<=num;i++){
       if(num%i==0) cout<<i<<" ";
   }
 }  
