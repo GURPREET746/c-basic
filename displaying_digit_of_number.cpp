f0f9842ffa53100ea75b303d5cb045cc219b3a99
GURPREET746 <60273071+GURPREET746@users.noreply.github.com>
2020-07-17 08:32:04 +0530

    Update displaying_digit_of_number.cpp

--- displaying_digit_of_number.cpp
+++ displaying_digit_of_number.cpp
@@ -5,7 +5,7 @@ int main(){
    int num,digit;
    cout<<"Enter the number"<<endl;
    cin>>num;
-   where(num!=0){
+   while(num!=0){
        digit=num%10;
        cout<<"The digit is: "<<digit<<endl;
        num=num/10;
